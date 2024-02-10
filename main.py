b43780e29e23d1ccecda9ffaf535043b4a8c37f5
NUMNIMx <itspeemgmth@gmail.com>
2024-02-10 19:55:30 +0700

    add cps page to main

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-print('hello world++') 
-#readme home less update naahee
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/check_cps", methods=["POST"])
+def check_cps():
+    start_time = request.form["start_time"]
+    end_time = request.form["end_time"]
+    clicks = request.form["clicks"]
+
+    try:
+        cps = int(clicks) / (int(end_time) - int(start_time))
+    except ValueError:
+        cps = 0
+
+    return render_template("result.html", cps=cps)
+
+if __name__ == "__main__":
+    app.run(debug=True)
