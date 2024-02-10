e4beeda681a1bbf71cd7a32769c9141f64a9b276
NUMNIMx <itspeemgmth@gmail.com>
2024-02-10 20:07:36 +0700

    add contact page

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+contact_info = {
+    "email": "youremail@example.com",
+    "phone": "123-456-7890",
+    "github": "github.com/yourusername"
+}
 
-@app.route("/me")
+@app.route("/")
 def index():
     return render_template("index.html")
 
@@ -18,6 +23,10 @@ def check_cps():
         cps = 0
 
     return render_template("result.html", cps=cps)
+
+@app.route("/")
+def index():
+    return render_template("indexx.html", contact_info=contact_info)
 
 if __name__ == "__main__":
     app.run(debug=True)
