2c47c84e752a0ec24ee8d4f65880ab984f80be56
NUMNIMx <itspeemgmth@gmail.com>
2024-02-11 17:45:28 +0700

    fix some issue

--- presidents.py
+++ presidents.py
@@ -6,19 +6,13 @@ application = app
 
 presidents_list = convert_to_dict("presidents.csv")
 
-# create a list of tuples in which the first item is the number
-# (Presidency) and the second item is the name (President)
 pairs_list = []
 for p in presidents_list:
     pairs_list.append( (p['Presidency'], p['President']) )
 
-# first route
-
 @app.route('/')
 def index():
     return render_template('index.html', pairs=pairs_list, the_title="Presidents Index")
-
-# second route
 
 @app.route('/president/<num>')
 def detail(num):
@@ -26,11 +20,8 @@ def detail(num):
         pres_dict = presidents_list[int(num) - 1]
     except:
         return f"<h1>Invalid value for Presidency: {num}</h1>"
-    # a little bonus function, imported on line 2 above
     ord = make_ordinal( int(num) )
     return render_template('president.html', pres=pres_dict, ord=ord, the_title=pres_dict['President'])
 
-
-# keep this as is
 if __name__ == '__main__':
     app.run(debug=True)
