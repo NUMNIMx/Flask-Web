26fc1e7b0c1de774053909ea9a9de32babbe5b1e
NUMNIMx <itspeemgmth@gmail.com>
2024-02-11 17:44:43 +0700

    remove unused data

--- modules.py
+++ modules.py
@@ -1,33 +1,20 @@
-"""
-functions to be imported into the presidents Flask app
-"""
-
 import csv
 
 def convert_to_dict(filename):
-    """
-    Convert a CSV file to a list of Python dictionaries
-    """
-    # open a CSV file - note - must have column headings in top row
+    
     datafile = open(filename, newline='')
 
-    # create DictReader object
     my_reader = csv.DictReader(datafile)
 
-    # create a regular Python list containing dicts
     list_of_dicts = list(my_reader)
 
-    # close original csv file
     datafile.close()
 
-    # return the list
     return list_of_dicts
 
 
 def make_ordinal(num):
-    """
-    Create an ordinal (1st, 2nd, etc.) from a number.
-    """
+
     base = num % 10
     if base in [0,4,5,6,7,8,9] or num in [11,12,13]:
         ext = "th"
@@ -39,7 +26,6 @@ def make_ordinal(num):
         ext = "rd"
     return str(num) + ext
 
-# tryouts
 def test_make_ordinal():
     for i in range(1,46):
         print(make_ordinal(i))
@@ -51,7 +37,6 @@ def search_the_list(list):
     for k in list[0].keys():
         print(k)
 
-# run tryouts
 if __name__ == '__main__':
     test_make_ordinal()
     presidents_list = convert_to_dict("presidents.csv")
